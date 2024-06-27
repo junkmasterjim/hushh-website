72160d9357586ea3dbaa067a3307b87b78584294
Noah Pittman <noahpittman00@gmail.com>
2024-06-27 18:57:15 -0300

    update dl link

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -2,40 +2,45 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const Download = () => {
-	return (
-		<>
-			<section className="container flex flex-col justify-between items-center h-full pb-24">
-				<div className="flex flex-col items-center justify-ceter py-12 sm:pt-24 max-w-prose mx-auto text-center">
-					<h3 className="text-5xl font-semibold">
-						download <span className="line-through">hushh</span>
-					</h3>
-					<p className="mt-4">Available free for MacOS.</p>
-					<p className="text-muted-foreground text-xs">version 11.0.0+</p>
+  return (
+    <>
+      <section className="container flex flex-col justify-between items-center h-full pb-24">
+        <div className="flex flex-col items-center justify-ceter py-12 sm:pt-24 max-w-prose mx-auto text-center">
+          <h3 className="text-5xl font-semibold">
+            download <span className="line-through">hushh</span>
+          </h3>
+          <p className="mt-4">Available free for MacOS.</p>
+          <p className="text-muted-foreground text-xs">version 11.0.0+</p>
 
-					<div className="mt-12">
-						<Link download={"/hushh.zip"} href={"/hushh.zip"}>
-							<Button>Download hushh for free</Button>
-						</Link>
-					</div>
-					<Link href={"/"} className="mt-8">
-						<Button variant={"ghost"} size={"sm"} className="text-xs">
-							Go back
-						</Button>
-					</Link>
-				</div>
+          <div className="mt-12">
+            <Link
+              href={
+                "https://github.com/noahpittman/hushh/releases/download/app-v1.1.0/hushh_1.1.0_x64.dmg"
+              }
+              target="_blank"
+            >
+              <Button>Download hushh for free</Button>
+            </Link>
+          </div>
+          <Link href={"/"} className="mt-8">
+            <Button variant={"ghost"} size={"sm"} className="text-xs">
+              Go back
+            </Button>
+          </Link>
+        </div>
 
-				<div className="text-center flex flex-col items-center justify-center gap-2">
-					<Link
-						href={"https://npitt.dev"}
-						target="_blank"
-						className="leading-5 hover:underline text-muted-foreground"
-					>
-						made with 🥩 by noah pittman
-					</Link>
-				</div>
-			</section>
-		</>
-	);
+        <div className="text-center flex flex-col items-center justify-center gap-2">
+          <Link
+            href={"https://npitt.dev"}
+            target="_blank"
+            className="leading-5 hover:underline text-muted-foreground"
+          >
+            made with 🥩 by noah pittman
+          </Link>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Download;
